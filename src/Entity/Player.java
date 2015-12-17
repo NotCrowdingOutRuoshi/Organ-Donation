5a49233f9377f2e97026dedb045a4a5033c4ac2f
Jia Ching Jian <sagiters1128@gmail.com>
2015-12-17 22:43:24 +0800

    Modify the README and Entity attribute

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -5,11 +5,19 @@ import java.util.Vector;
 import Enum.SpriteType;
 
 public class Player extends Sprite {
-
+	
 	private int direction;
 	private int velocity;
+	private String name;
 	private Vector<Item> itmes;
 
+	public Player(int clientId,String username,int locationX,int locationY) {
+		id = clientId;
+		name = username;
+		locationX = x;
+		locationY = y;
+	}
+	
 	public int getDirection() {
 		return direction;
 	}
@@ -18,14 +26,31 @@ public class Player extends Sprite {
 		return velocity;
 	}
 
+	public String getName() {
+		return name;
+	}
+	
 	public Vector<Item> getOwnItems() {
 		return itmes;
 	}
 
 	@Override
-	SpriteType getType() {
+	public SpriteType getType() {
 		// TODO Auto-generated method stub
 		return SpriteType.PLAYER;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return 
+				getType().toString() + " " + 
+				String.valueOf(id) + " " + 
+				name + " " + 
+				String.valueOf(x) + " " +
+				String.valueOf(y) + " " + 
+				String.valueOf(direction) + " " +
+				String.valueOf(velocity);
 	}
 
 }
