8351bf730858d1a37b2885a9b250acc136a04353
Jia Ching Jian <sagiters1128@gmail.com>
2015-12-31 15:04:08 +0800

    Add get & set GameState function to CDC and ICDC

--- src/CentralizedDataCenter/CDC.java
+++ src/CentralizedDataCenter/CDC.java
@@ -2,6 +2,7 @@ package CentralizedDataCenter;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Vector;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -13,86 +14,88 @@ import Common.StateType;
 import Common.Interfaces.ICentralizedDataCenter;
 
 public class CDC implements ICentralizedDataCenter {
-	Map<Integer,Player> PlayerList;
-	Map<Integer,Organ> OrganList;
+	Map<Integer, Player> PlayerList;
+	Map<Integer, Organ> OrganList;
+	private String _gameState = Constants.GAME_STATE_MENU;
+	private Vector<String> gameStateList;
 
-	public CDC(){
-		PlayerList = new HashMap<Integer,Player>();
-		OrganList = new HashMap<Integer,Organ>();
+	public CDC() {
+		PlayerList = new HashMap<Integer, Player>();
+		OrganList = new HashMap<Integer, Organ>();
+		gameStateList = new Vector<>();
+		gameStateList.add(Constants.GAME_STATE_INIT);
+		gameStateList.add(Constants.GAME_STATE_MENU);
+		gameStateList.add(Constants.GAME_STATE_START);
+		gameStateList.add(Constants.GAME_STATE_OVER);
+		gameStateList.add(Constants.GAME_STATE_WAIT);
 	}
-	
-	public void addPlayer(int id){
+
+	public void addPlayer(int id) {
 		Player player = new Player();
-		player.setID(id);		
+		player.setID(id);
 		PlayerList.put(id, player);
 	}
 
-	public void updateDir(int id ,int dir){
+	public void updateDir(int id, int dir) {
 		Player player = PlayerList.get(id);
 		player.setDir(dir);
-		
+
 	}
-	
-	public void setState(int id,String state){
+
+	public void setState(int id, String state) {
 		Player player = PlayerList.get(id);
 		player.setState(state);
-		if(state.equals(StateType.ATTACK)){
+		if (state.equals(StateType.ATTACK)) {
 			playerAttack(player);
-		}
-		else if(state.equals(StateType.STEAL)){
+		} else if (state.equals(StateType.STEAL)) {
 			playerSteal(player);
 		}
 	}
 
-	private void playerAttack(Player player){
+	private void playerAttack(Player player) {
 		int targetX = player.getX();
 		int targetY = player.getY();
-		
-		if(player.getDir()==Constants.ACTIONCODE_NORTH){
+
+		if (player.getDir() == Constants.ACTIONCODE_NORTH) {
 			targetY -= 1;
-		}
-		else if(player.getDir()==Constants.ACTIONCODE_SOUTH){
+		} else if (player.getDir() == Constants.ACTIONCODE_SOUTH) {
 			targetY += 1;
-		}
-		else if(player.getDir()==Constants.ACTIONCODE_EAST){
+		} else if (player.getDir() == Constants.ACTIONCODE_EAST) {
 			targetX += 1;
-		}
-		else if(player.getDir()==Constants.ACTIONCODE_WEST){
+		} else if (player.getDir() == Constants.ACTIONCODE_WEST) {
 			targetX -= 1;
 		}
-		
-		for(int i=1;i<=PlayerList.size();i++){
-			if(i!=player.getId()){
+
+		for (int i = 1; i <= PlayerList.size(); i++) {
+			if (i != player.getId()) {
 				Player player2 = PlayerList.get(i);
-				if(targetX==player2.getX() && targetY==player2.getY()){
-					player2.setHealth(player2.getHealth()-50);
+				if (targetX == player2.getX() && targetY == player2.getY()) {
+					player2.setHealth(player2.getHealth() - 50);
 				}
 			}
 		}
-		player.setHealth(player.getHealth()-5);
+		player.setHealth(player.getHealth() - 5);
 	}
-	
-	private void playerSteal(Player player){
+
+	private void playerSteal(Player player) {
 		int targetX = player.getX();
 		int targetY = player.getY();
-		
-		if(player.getDir()==Constants.ACTIONCODE_NORTH){
+
+		if (player.getDir() == Constants.ACTIONCODE_NORTH) {
 			targetY -= 1;
-		}
-		else if(player.getDir()==Constants.ACTIONCODE_SOUTH){
+		} else if (player.getDir() == Constants.ACTIONCODE_SOUTH) {
 			targetY += 1;
-		}
-		else if(player.getDir()==Constants.ACTIONCODE_EAST){
+		} else if (player.getDir() == Constants.ACTIONCODE_EAST) {
 			targetX += 1;
-		}
-		else if(player.getDir()==Constants.ACTIONCODE_WEST){
+		} else if (player.getDir() == Constants.ACTIONCODE_WEST) {
 			targetX -= 1;
 		}
-		
-		for(int i=1;i<=PlayerList.size();i++){
-			if(i!=player.getId()){
+
+		for (int i = 1; i <= PlayerList.size(); i++) {
+			if (i != player.getId()) {
 				Player player2 = PlayerList.get(i);
-				if(targetX==player2.getX() && targetY==player2.getY() && player2.getState().equals(StateType.EXHAUST)){
+				if (targetX == player2.getX() && targetY == player2.getY()
+						&& player2.getState().equals(StateType.EXHAUST)) {
 					Organ o = player2.StealedOrgan();
 					player2.remove(o);
 					player.addOrgan(o);
@@ -100,103 +103,108 @@ public class CDC implements ICentralizedDataCenter {
 			}
 		}
 	}
-	
-	public String getUpdateInfo(){
+
+	public String getUpdateInfo() {
 		JSONArray j = new JSONArray(PlayerList.values());
 		return j.toString();
 	}
-	
-	private void computingXYThread(){
 
-		Thread thread = new Thread()
-	    {
-	        public void run()
-	        {
-	        	while(true){	
-	        		try {
+	private void computingXYThread() {
+
+		Thread thread = new Thread() {
+			public void run() {
+				while (true) {
+					try {
 						Thread.sleep(500);
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					} 
-	        		for(int i=0;i<PlayerList.size();i++){
-	        			Player player = PlayerList.get(i+1);
-	        			if(player.getState().equals(StateType.WALK)){
-	        				if(player.getDir()==Constants.ACTIONCODE_NORTH){
-	        					player.setY(player.getY()-player.getSpeed()/2);
-	        				}
-	        				else if(player.getDir()==Constants.ACTIONCODE_SOUTH){
-	        					player.setY(player.getY()+player.getSpeed()/2);
-	        				}
-	        				else if(player.getDir()==Constants.ACTIONCODE_EAST){
-	        					player.setX(player.getX()+player.getSpeed()/2);
-	        				}
-	        				else if(player.getDir()==Constants.ACTIONCODE_WEST){
-	        					player.setX(player.getX()-player.getSpeed()/2);
-	        				}
-	        				
-	        			}
-	        					
-	        		}
-	        	}
-	        	
-	        }
-	    };
-	    thread.start();
-	}
-	
-	public void GameLogicThread(){
+					}
+					for (int i = 0; i < PlayerList.size(); i++) {
+						Player player = PlayerList.get(i + 1);
+						if (player.getState().equals(StateType.WALK)) {
+							if (player.getDir() == Constants.ACTIONCODE_NORTH) {
+								player.setY(player.getY() - player.getSpeed() / 2);
+							} else if (player.getDir() == Constants.ACTIONCODE_SOUTH) {
+								player.setY(player.getY() + player.getSpeed() / 2);
+							} else if (player.getDir() == Constants.ACTIONCODE_EAST) {
+								player.setX(player.getX() + player.getSpeed() / 2);
+							} else if (player.getDir() == Constants.ACTIONCODE_WEST) {
+								player.setX(player.getX() - player.getSpeed() / 2);
+							}
 
-		Thread thread = new Thread()
-	    {
-	        public void run()
-	        {
-	        	while(true){
-	        		try {
+						}
+
+					}
+				}
+
+			}
+		};
+		thread.start();
+	}
+
+	public void GameLogicThread() {
+
+		Thread thread = new Thread() {
+			public void run() {
+				while (true) {
+					try {
 						Thread.sleep(500);
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					};
-	        		enviromentLogic();
-	        		logic();
-	        		gameover();
-	        	}
-	        	
-	        }
-	    };
-	    thread.start();
+					}
+					;
+					enviromentLogic();
+					logic();
+				}
+
+			}
+		};
+		thread.start();
 	}
-	
-	public void logic(){
+
+	public void logic() {
 		int alive = PlayerList.size();
-		for(int i=1;i<=PlayerList.size();i++){
+		for (int i = 1; i <= PlayerList.size(); i++) {
 			Player player = PlayerList.get(i);
-			if(player.getHealth()==0){
+			if (player.getHealth() == 0) {
 				player.setState(StateType.EXHAUST);
 			}
-			if(player.getEnergy()==0){
+			if (player.getEnergy() == 0) {
 				player.setState(StateType.DEATH);
 			}
-			if(player.getState().equals(StateType.DEATH)){
+			if (player.getState().equals(StateType.DEATH)) {
 				alive -= 1;
-				if(alive<=1){
-					System.out.println("gameover");
+				if (alive <= 1) {
+					setGameState(Constants.GAME_STATE_OVER);
 				}
 			}
 		}
 	}
-	
-	public void enviromentLogic(){
-		for(int i=1;i<=PlayerList.size();i++){
+
+	public void enviromentLogic() {
+		for (int i = 1; i <= PlayerList.size(); i++) {
 			Player player = PlayerList.get(i);
 			System.out.println(getUpdateInfo());
 			player.decreaseOrganHp(20);
 		}
 	}
-	
-	public void gameover(){
-		//send gameover
+
+	@Override
+	public void setGameState(String gameState) {
+		// TODO Auto-generated method stub
+		if (gameStateList.contains(gameState)) {
+			_gameState = gameState;
+		} else {
+			assert false;
+		}
+	}
+
+	@Override
+	public String getGameState() {
+		// TODO Auto-generated method stub
+		return _gameState;
 	}
 
 }
