c6d30ffa0495af33ddb1d154b964ef874dbf2cd3
WeiJhe <sky820902@gmail.com>
2015-12-31 16:36:50 +0800

    add TCPServer broadcast method's parameter

--- src/Net/TCP/Server/TCPServer.java
+++ src/Net/TCP/Server/TCPServer.java
@@ -18,28 +18,28 @@ public class TCPServer implements Runnable, ITCPServer {
 	private ServerSocket serverSocket;
 
 	private Vector<InetAddress> clientIPTable;
-	
+
 	private Vector<Socket> clientOut;
-	
+
 	private ICentralizedDataCenter cdc;
-	
+
 	public TCPServer(ICentralizedDataCenter cdc) {
 		this.cdc = cdc;
 		clientIPTable = new Vector<InetAddress>();
 		clientOut = new Vector<Socket>();
 	}
-	
+
 	private void initTCPServer() throws IOException {
 		serverSocket = new ServerSocket(9090);
-		while(true){
+		while (true) {
 			Socket socket = serverSocket.accept();
-			new ConnectThread(socket,cdc).start();
+			new ConnectThread(socket, cdc).start();
 			clientIPTable.add(socket.getInetAddress());
 			clientOut.add(socket);
 		}
 	}
-	
-	public void startServer(){
+
+	public void startServer() {
 		Thread thread = new Thread(this);
 		thread.start();
 	}
@@ -47,16 +47,16 @@ public class TCPServer implements Runnable, ITCPServer {
 	public Vector<InetAddress> getClientIPTable() {
 		return clientIPTable;
 	}
-	
-	public void BroadcastAllClient() throws IOException{
-		assert(serverSocket!=null);
-		for( int i=0; i<clientOut.size(); i++ ){
+
+	public void BroadcastAllClient(String state) throws IOException {
+		assert (serverSocket != null);
+		for (int i = 0; i < clientOut.size(); i++) {
 			OutputStream out = clientOut.get(i).getOutputStream();
-			out.write(11);
+			out.write(state.getBytes());
 			out.flush();
 		}
 	}
-	
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
@@ -79,14 +79,14 @@ public class TCPServer implements Runnable, ITCPServer {
 class ConnectThread extends Thread {
 
 	private Socket socket;
-	
+
 	private ICentralizedDataCenter cdc;
-	
+
 	private int playerId;
-	
+
 	private static int playerCnt = 0;
 
-	public ConnectThread(Socket socket,ICentralizedDataCenter cdc) {
+	public ConnectThread(Socket socket, ICentralizedDataCenter cdc) {
 		this.socket = socket;
 		this.cdc = cdc;
 		playerCnt++;
@@ -96,36 +96,38 @@ class ConnectThread extends Thread {
 	@Override
 	public void run() {
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
 			OutputStream out = socket.getOutputStream();
-			
+
 			out.write(playerId);
 			out.flush();
-			
+
 			cdc.addPlayer(playerId);
-			
+
 			while (true) {
 				String[] recv = br.readLine().split(" ");
 				int clientId = Integer.valueOf(recv[0]);
-				String actionState = recv[recv.length-1]; 
-				
+				String actionState = recv[recv.length - 1];
+
 				switch (actionState) {
-					case StateType.WALK:
-						cdc.updateDir(clientId, Integer.valueOf(recv[1]));
-						cdc.setState(clientId, actionState);
-						break;
-					case StateType.ATTACK:
-						cdc.setState(clientId, actionState);
-						break;
-					case StateType.STEAL:
-						cdc.setState(clientId, actionState);
-						break;
-					case StateType.IDLE:
-						cdc.setState(clientId, actionState);
-						break;
-					default:
-						System.out.println(clientId+"send illegal message"+recv+" !!!!!!!!!");
-						break;
+				case StateType.WALK:
+					cdc.updateDir(clientId, Integer.valueOf(recv[1]));
+					cdc.setState(clientId, actionState);
+					break;
+				case StateType.ATTACK:
+					cdc.setState(clientId, actionState);
+					break;
+				case StateType.STEAL:
+					cdc.setState(clientId, actionState);
+					break;
+				case StateType.IDLE:
+					cdc.setState(clientId, actionState);
+					break;
+				default:
+					System.out.println(clientId + "send illegal message" + recv
+							+ " !!!!!!!!!");
+					break;
 				}
 			}
 		} catch (IOException e) {
