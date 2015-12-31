c9928c008d4c298ff54a1c28eb79d66040bae78d
Jia Ching Jian <sagiters1128@gmail.com>
2015-12-31 14:35:49 +0800

    Add GameState Constants

--- src/Common/Constants.java
+++ src/Common/Constants.java
@@ -6,4 +6,12 @@ public class Constants {
 	public static int ACTIONCODE_NORTH = 38; 
 	public static int ACTIONCODE_SOUTH = 40;
 	public static int ACTIONCODE_GET = 32;
+	
+	public static final String GAME_STATE = "GameState";
+	public static final String GAME_STATE_MENU = "GameMenu";
+	public static final String GAME_STATE_START = "GameStart";
+	public static final String GAME_STATE_INIT = "GameInit";
+	public static final String GAME_STATE_WAIT = "GameWait";
+	public static final String GAME_STATE_OVER = "GameOver";
+	
 }
