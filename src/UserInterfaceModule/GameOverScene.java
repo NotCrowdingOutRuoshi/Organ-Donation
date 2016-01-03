ee8c498fd7600c32e6dd32505ba39f563c01af03
wanchuan <wanchuan123@gmail.com>
2016-01-03 17:30:07 +0800

    Add backgroud to menuscene waitscene gameoverscene

--- src/UserInterfaceModule/GameOverScene.java
+++ src/UserInterfaceModule/GameOverScene.java
@@ -1,8 +1,15 @@
 package UserInterfaceModule;
 import javax.swing.JPanel;
 
-import java.awt.Font;
+import Resources.Resources;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 public class GameOverScene extends JPanel {
@@ -12,11 +19,22 @@ public class GameOverScene extends JPanel {
 	 */
 	public GameOverScene() {
 		this.setSize(1450, 850);
-		
+		this.setLayout(null);
 		JLabel lblGameOver = new JLabel("GAMEOVER ! Winner is Player "+GameManager.getInstance().getWinnerId());
-		lblGameOver.setBounds((this.getWidth()-100)/2, 20, 100, 100);
+		lblGameOver.setBounds((this.getWidth()-800)/2, 300, 800, 100);
 		lblGameOver.setFont(new Font("Serif", Font.PLAIN, 50));
+		lblGameOver.setForeground(Color.white);
 		add(lblGameOver);
 	}
-
+	public void paintComponent(Graphics g){
+		 
+		 try {
+			ImageIcon icon=new ImageIcon(ImageIO.read(Resources.getResourceStream("Scene/menu.jpg")));
+			 g.drawImage(icon.getImage(),0,0,getSize().width,getSize().height,this);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	       
+	 }
 }
